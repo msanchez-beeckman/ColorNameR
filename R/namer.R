2cbd711bdc7936bc3f7823bfc3c4e6a529d2a5ab
Marco Sánchez <msanchezb3@gmail.com>
2021-06-23 17:03:41 +0200

    Add support for naming vectors of colors and different color spaces

--- R/namer.R
+++ R/namer.R
@@ -5,8 +5,8 @@
 #' @param a The chromatic component a* (red-green).
 #' @param b The chromatic component b* (blue-yellow).
 #' @return A one-row tibble
-#' @export
 #' @importFrom magrittr %>%
+#' @export
 #' @examples
 #' get_closest_color(65, 20, -20)
 get_closest_color <- function(L, a, b) {
@@ -16,19 +16,25 @@ get_closest_color <- function(L, a, b) {
     dplyr::slice_min(lab_diff)
 }
 
-#' Name a color given its coordinates in the CIELab color space.
+#' Name a color given its coordinates in a specified color space.
 #'
-#' @param L The lightness L* of the color.
-#' @param a The chromatic component a* (red-green).
-#' @param b The chromatic component b* (blue-yellow).
+#' @param color A matrix whose rows specify colors.
+#' @param colorspace The color space the coordinates of the colors are in.
+#' @param illuminant The reference white, or `NULL` if not needed.
 #' @param language The language of the color name, between English, French, German, and Spanish.
 #' @return The name of the color, according to the UPOV.
+#' @importFrom grDevices convertColor
+#' @details
+#' The available color spaces are `"XYZ"`, `"sRGB"`, `"Apple RGB"`, `"CIE RGB"`, `"Luv"`, and `"Lab"` (default).
+#' If the color space is an RGB variant, the coordinates must take values between 0 and 1.
 #' @export
 #' @examples
-#' name_color(65, 20, -20)
-#' name_color(65, 20, -20, "spanish")
-#' name_color(65, 20, -20, "es")
-name_color <- function(L, a, b, language="english") {
+#' name(c(65, 20, -20))
+#' name(c(65, 20, -20), language="Spanish")
+#' name(c(65, 20, -20), language="es")
+#' name(c(244/255, 234/255, 184/255), colorspace="sRGB")
+#' name(rbind(c(65, 20, -20), c(69, 4, -31)))
+name <- function(color, colorspace="Lab", illuminant=NULL, language="english") {
   language <- switch(tolower(language),
                      en="english",
                      english="english",
@@ -42,5 +48,11 @@ name_color <- function(L, a, b, language="english") {
                      spanish="español",
                      español="español")
   if (is.null(language)) stop("Unavailable language")
-  get_closest_color(L, a, b)[[{{ language }}]]
+  color %>%
+    convertColor(from={{ colorspace }}, from.ref.white={{ illuminant }},
+                 to="Lab", to.ref.white="D65") %>%
+    purrr::array_branch(1) %>%
+    purrr::map_chr(function (c) {
+      get_closest_color(c[1], c[2], c[3])[[ {{ language }}]]
+    })
 }
--- tests/testthat/test-namer.R
+++ tests/testthat/test-namer.R
@@ -3,38 +3,55 @@ test_that("correct names are given in different languages", {
   L <- 93
   a <- -9
   b <- 36
-  expect_equal(name_color(L, a, b), "light yellow green")
-  expect_equal(name_color(L, a, b, "french"), "vert-jaune clair")
-  expect_equal(name_color(L, a, b, "german"), "hellgelbgrün")
-  expect_equal(name_color(L, a, b, "spanish"), "verde amarillento claro")
+  color1 <- c(L, a, b)
+  expect_equal(name(color1), "light yellow green")
+  expect_equal(name(color1, language="french"), "vert-jaune clair")
+  expect_equal(name(color1, language="german"), "hellgelbgrün")
+  expect_equal(name(color1, language="spanish"), "verde amarillento claro")
 
   L2 <- 69
   a2 <- 4
   b2 <- -31
-  expect_equal(name_color(L2, a2, b2), "light blue")
-  expect_equal(name_color(L2, a2, b2, "french"), "bleu clair")
-  expect_equal(name_color(L2, a2, b2, "german"), "hellblau")
-  expect_equal(name_color(L2, a2, b2, "spanish"), "azul claro")
+  color2 <- c(L2, a2, b2)
+  expect_equal(name(color2), "light blue")
+  expect_equal(name(color2, language="french"), "bleu clair")
+  expect_equal(name(color2, language="german"), "hellblau")
+  expect_equal(name(color2, language="spanish"), "azul claro")
 })
 
 test_that("asking for a color name in an unknown language raises an error", {
   L <- 93
   a <- -9
   b <- 36
-  expect_error(name_color(L, a, b, "sumerian"))
+  color1 <- c(L, a, b)
+  expect_error(name(color1, language="sumerian"))
 })
 
 test_that("shorthands for languages work correctly", {
   L <- 93
   a <- -9
   b <- 36
-  expect_equal(name_color(L, a, b, "en"), name_color(L, a, b))
-  expect_equal(name_color(L, a, b, "en"), name_color(L, a, b, "english"))
-  expect_equal(name_color(L, a, b, "fr"), name_color(L, a, b, "french"))
-  expect_equal(name_color(L, a, b, "fr"), name_color(L, a, b, "français"))
-  expect_equal(name_color(L, a, b, "de"), name_color(L, a, b, "german"))
-  expect_equal(name_color(L, a, b, "de"), name_color(L, a, b, "deutsch"))
-  expect_equal(name_color(L, a, b, "es"), name_color(L, a, b, "spanish"))
-  expect_equal(name_color(L, a, b, "es"), name_color(L, a, b, "español"))
+  color1 <- c(L, a, b)
+  expect_equal(name(color1, language="en"), name(color1))
+  expect_equal(name(color1, language="en"), name(color1, language="english"))
+  expect_equal(name(color1, language="fr"), name(color1, language="french"))
+  expect_equal(name(color1, language="fr"), name(color1, language="français"))
+  expect_equal(name(color1, language="de"), name(color1, language="german"))
+  expect_equal(name(color1, language="de"), name(color1, language="deutsch"))
+  expect_equal(name(color1, language="es"), name(color1, language="spanish"))
+  expect_equal(name(color1, language="es"), name(color1, language="español"))
+})
+
+test_that("asking for the names of multiple colors returns multiple names", {
+  color1 <- c(93, -9, 36)
+  color2 <- c(69, 4, -31)
+  color3 <- c(45, -20, -30)
+  colors <- rbind({{ color1 }}, {{ color2 }}, {{ color3 }})
+  expect_equal(name(colors), c("light yellow green", "light blue", "medium green blue"))
+})
+
+test_that("the transformation from sRGB to Lab works correctly", {
+  colors <- rbind(c(244/255, 234/255, 184/255), c(243/255, 231/255, 103/255))
+  expect_equal(name(colors, colorspace="sRGB"), c("light yellow", "medium yellow"))
 })
 
